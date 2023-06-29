e603479cba247d32d953c2d34066533d83ffb205
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-29 18:06:21 +0200

    use namedtuple instead of struct for params

--- dynamics/julia/demos/quad_2d/.ipynb_checkpoints/trajectory_generation-checkpoint.jl
+++ dynamics/julia/demos/quad_2d/.ipynb_checkpoints/trajectory_generation-checkpoint.jl
@@ -1,28 +1,20 @@
-abstract type Trajectory end
 
-struct CircleTrajectory2 <: Trajectory 
-    r::Float64       # radius [m]
-    ω::Float64       # angular velocity [rad/s]
-    tspan::Tuple     # [seconds] eg. (0.0,10.0)
-    dt::Float64      # [seconds] 
-end
+function generate_circle_trajectory(trajec::NamedTuple)
 
-CircleTrajectory = CircleTrajectory2
+    # Getting value using Key
+    tspan = getindex(trajec, :tspan)
+    dt = getindex(trajec, :dt)
+    r = getindex(trajec, :r)
+    ω = getindex(trajec, :ω)
 
-
-function generate_trajectory(trajec::CircleTrajectory)
-
-    t_vec = trajec.tspan[1]:trajec.dt:trajec.tspan[2]
-
-    # x_vec = Vector{Float64}(undef, size(t_vec)) 
-    # y_vec = Vector{Float64}(undef, size(t_vec)) 
+    t_vec = tspan[1]:dt:tspan[2]
 
     x_vec = Float64[]
     y_vec = Float64[]
         
     for t in t_vec
-        x::Float64 = trajec.r * cos(trajec.ω*t)
-        y::Float64 = trajec.r * sin(trajec.ω*t)
+        x::Float64 = r * cos(ω*t)
+        y::Float64 = r * sin(ω*t)
     
         push!(x_vec, x)
         push!(y_vec, y)
@@ -30,3 +22,4 @@ function generate_trajectory(trajec::CircleTrajectory)
 
     return [x_vec, y_vec]
 end
+
