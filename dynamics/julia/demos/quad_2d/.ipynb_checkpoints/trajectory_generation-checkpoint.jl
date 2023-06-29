a3c8483b5ddcf6d8d0a02090def8e7688f191565
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-29 17:40:01 +0200

    added circle trajectory

--- dynamics/julia/demos/quad_2d/.ipynb_checkpoints/trajectory_generation-checkpoint.jl
+++ dynamics/julia/demos/quad_2d/.ipynb_checkpoints/trajectory_generation-checkpoint.jl
@@ -1,11 +1,28 @@
-function circle_trajectory(r::Float64, ω::Float64, dt::Float64, tspan::Tuple)
-    
+abstract type Trajectory end
+
+struct CircleTrajectory2 <: Trajectory 
+    r::Float64       # radius [m]
+    ω::Float64       # angular velocity [rad/s]
+    tspan::Tuple     # [seconds] eg. (0.0,10.0)
+    dt::Float64      # [seconds] 
+end
+
+CircleTrajectory = CircleTrajectory2
+
+
+function generate_trajectory(trajec::CircleTrajectory)
+
+    t_vec = trajec.tspan[1]:trajec.dt:trajec.tspan[2]
+
+    # x_vec = Vector{Float64}(undef, size(t_vec)) 
+    # y_vec = Vector{Float64}(undef, size(t_vec)) 
+
     x_vec = Float64[]
     y_vec = Float64[]
         
-    for t in tspan[1]:dt:tspan[2]
-        x = r * cos(ω*t)
-        y = r * sin(ω*t)
+    for t in t_vec
+        x::Float64 = trajec.r * cos(trajec.ω*t)
+        y::Float64 = trajec.r * sin(trajec.ω*t)
     
         push!(x_vec, x)
         push!(y_vec, y)
