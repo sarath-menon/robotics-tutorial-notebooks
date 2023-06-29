626c7a689c7593dcd265a7482e662cc9629b8cc5
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-30 01:04:07 +0200

    trajectory tracking works

--- dynamics/julia/demos/quad_2d/utilities.jl
+++ dynamics/julia/demos/quad_2d/utilities.jl
@@ -1,4 +1,4 @@
-function quad_2d_plot_normal(sol::ODESolution)
+function quad_2d_plot_normal(sol::ODESolution;y_ref, z_ref, theta_ref)
     fig = Figure(resolution=(1200, 600))
 
     # state variables 
@@ -10,9 +10,14 @@ function quad_2d_plot_normal(sol::ODESolution)
     thrust_ax = Axis(fig[1, 2], title="Thrust")
     torque_ax = Axis(fig[2, 2], title="Torque")
 
-    lines!(x_ax, sol.t, sol[1, :])
+    lines!(y_ax, sol.t, sol[1, :])
+    lines!(z_ax, sol.t, z_ref,linestyle = :dash)
+    
     lines!(z_ax, sol.t, sol[2, :])
+    lines!(y_ax, sol.t, y_ref,linestyle = :dash)
+    
     lines!(theta_ax, sol.t[:], sol[3, :])
+    lines!(theta_ax, sol.t, theta_ref,linestyle = :dash)
 
     f_1 = sol[7, :]
     f_2 = sol[8, :]
@@ -29,6 +34,44 @@ function quad_2d_plot_normal(sol::ODESolution)
 
     thrust_ax.ylabel = "Thrust [N]"
     torque_ax.ylabel = "Toruqe [Nm]"
+
+
+    rowgap!(fig.layout, 1)
+
+    fig
+end
+
+function quad_2d_plot_reference(sol::ODESolution;y_ref, z_ref, theta_ref)
+    fig = Figure(resolution=(1200, 600))
+
+    # state variables 
+    y_ax = Axis(fig[1, 1], title="y reference")
+    z_ax = Axis(fig[2, 1], title="z reference")
+    theta_ax = Axis(fig[3, 1], title="theta reference")
+
+    # # control input 
+    # thrust_ax = Axis(fig[1, 2], title="Thrust")
+    # torque_ax = Axis(fig[2, 2], title="Torque")
+
+    lines!(y_ax, sol.t, y_ref)
+    lines!(z_ax, sol.t, z_ref)
+    lines!(theta_ax, sol.t, theta_ref)
+
+    # f_1 = sol[7, :]
+    # f_2 = sol[8, :]
+
+    # thrust = f_1 + f_2
+    # torque = (f_1 - f_2) * 0.1
+
+    # lines!(thrust_ax, sol.t, thrust)
+    # lines!(torque_ax, sol.t, torque )
+
+    y_ax.ylabel = "y [m]"
+    z_ax.ylabel = "z [m]"
+    theta_ax.ylabel = "θ [rad]"
+
+    # thrust_ax.ylabel = "Thrust [N]"
+    # torque_ax.ylabel = "Toruqe [Nm]"
 
 
     rowgap!(fig.layout, 1)
