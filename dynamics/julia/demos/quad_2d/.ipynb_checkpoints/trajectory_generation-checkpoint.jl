4c1fd72eb61de3fc8ccc898638a5e24583f826d0
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-30 15:53:26 +0200

    fixed trajectory generator

--- dynamics/julia/demos/quad_2d/.ipynb_checkpoints/trajectory_generation-checkpoint.jl
+++ dynamics/julia/demos/quad_2d/.ipynb_checkpoints/trajectory_generation-checkpoint.jl
@@ -60,33 +60,35 @@ function generate_circle_trajectory(trajec::NamedTuple, t::Vector{Float64})
 end
 
 
-function generate_circle_trajectory(trajec::NamedTuple, t::Float64)
-
-    # Getting value using Key
-    tspan = getindex(trajec, :tspan)
+function generate_circle_trajectory(trajec_params::NamedTuple, quad_params::NamedTuple, t::Float64)
+    m::Float64 = quad_params.m
     
-    dt = getindex(trajec, :dt)
-    r = getindex(trajec, :r)
-    ω = getindex(trajec, :ω)
+    r::Float64 = trajec_params.r
+    ω::Float64 = trajec_params.ω
+    g::Float64 = trajec_params.g
 
-    y₀ = getindex(trajec, :y₀)
-    z₀ = getindex(trajec, :z₀)
+    tspan = trajec_params.tspan
+    y₀ = trajec_params.y₀
+    z₀ = trajec_params.z₀
 
     y::Float64 = (r*cos(ω*t)) + y₀
     z::Float64 = (r*sin(ω*t)) + z₀
 
-    # ẋ,ẏ = 0, since ω̇=0
     ẏ::Float64 = (-r*sin(ω*t))*ω
     ż::Float64 = (r*cos(ω*t))*ω
 
     ÿ::Float64 = -y * (ω^2)
     z̈::Float64 = -z * (ω^2)
-    
-    m::Float64 = 1.0
-    
-    # compute theta using differential flatness
-    θ::Float64 = atan(-m*ÿ, m*(z̈ + 9.81))
-    θ̇ = 0 
+ 
+
+    ÿ_func(r::Real,ω::Real,t::Real) = -r*cos(ω*t) * (ω^2)
+    z̈_func(r::Real,ω::Real,t::Real) = -r*sin(ω*t) * (ω^2)
+
+    θ_func(t::Real) = atan(-m * ÿ_func(r,ω,t), m*(z̈_func(r,ω,t) -g))
+
+    # compute θ, θ̇ using differential flatness
+    θ::Float64 =  θ_func(t)
+    θ̇::Float64 = ForwardDiff.derivative(θ_func, t) # constant value (precomputed)
     
     return [y, z, θ , ẏ , ż, θ̇]
 end
