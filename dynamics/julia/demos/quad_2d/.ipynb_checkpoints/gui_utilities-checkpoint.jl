267a437c9f95fb29b9c3a0c20ad6ee171fa1e29e
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-02 00:10:34 +0200

    better plots

--- dynamics/julia/demos/quad_2d/.ipynb_checkpoints/gui_utilities-checkpoint.jl
+++ dynamics/julia/demos/quad_2d/.ipynb_checkpoints/gui_utilities-checkpoint.jl
@@ -10,3 +10,25 @@ function to_makie_quaternion(R_euler::RotZYX)::Quaternion
     
     return  Quaternion(R_quat.q.s, R_quat.q.v1,R_quat.q.v2,R_quat.q.v3)
 end
+
+function plot_axis2d(axis::Axis; x::Vector{Float64}, y::Vector{Float64},  title::String)
+     # set title
+    axis.title = title
+
+    # fraw plot
+    lines!(axis, x, y)
+end
+
+function plot_trajectory(config_dict::Dict, config)
+    # diplayed plots
+    for (i,title) in enumerate(config_dict[config])
+        # clear axis
+        empty!(state_plots[i])
+
+        # plot actual trajectory 
+        plot_axis2d(state_plots[i]; x = df.timestamp, y = df[!,title], title=title)
+
+        # plot desired trajectory 
+        plot_axis2d(state_plots[i]; x = df.timestamp, y = df[!,title*"_req"], title=title)
+    end
+end
--- dynamics/julia/demos/quad_2d/gui_utilities.jl
+++ dynamics/julia/demos/quad_2d/gui_utilities.jl
@@ -1,12 +1,34 @@
 # convert standrard quaternion, euler angle to makie quaternin type
 function to_makie_quaternion(R_quat::QuatRotation)::Quaternion
-    return  Quaternion(R_quat.q.s, R_quat.q.v1,R_quat.q.v2,R_quat.q.v3)
+    return Quaternion(R_quat.q.s, R_quat.q.v1, R_quat.q.v2, R_quat.q.v3)
 end
 
 function to_makie_quaternion(R_euler::RotZYX)::Quaternion
-    
+
     # Convert the rotation to unit quaternion
     R_quat = QuatRotation(R_euler)
-    
-    return  Quaternion(R_quat.q.s, R_quat.q.v1,R_quat.q.v2,R_quat.q.v3)
+
+    return Quaternion(R_quat.q.s, R_quat.q.v1, R_quat.q.v2, R_quat.q.v3)
+end
+
+function plot_axis2d(axis::Axis; x::Vector{Float64}, y::Vector{Float64}, title::String, linestyle=nothing, color=:black, linewidth=3)
+    # set title
+    axis.title = title
+
+    # fraw plot
+    lines!(axis, x, y, linestyle=linestyle, color=color, linewidth=linewidth)
+end
+
+function plot_trajectory(config_dict::Dict, config)
+    # diplayed plots
+    for (i, title) in enumerate(config_dict[config])
+        # clear axis
+        empty!(state_plots[i])
+
+        # plot actual trajectory 
+        plot_axis2d(state_plots[i]; x=df.timestamp, y=df[!, title], title=title, linewidth=2)
+
+        # plot desired trajectory 
+        plot_axis2d(state_plots[i]; x=df.timestamp, y=df[!, title*"_req"], title=title, linestyle=:dash, color=:green, linewidth=6)
+    end
 end
