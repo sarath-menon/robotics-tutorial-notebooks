5f53b874e32bd9cf2a3a2507fe3d77a715912e21
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-07-02 11:19:38 +0200

    stop button function works

--- dynamics/julia/demos/quad_2d/gui_utilities.jl
+++ dynamics/julia/demos/quad_2d/gui_utilities.jl
@@ -33,41 +33,47 @@ function plot_trajectory(config_dict::Dict, config)
     end
 end
 
-function plot_3d_trajectory(; sim_time_obs::Observable, duration=10.0, dt=0.01, frame_rate=25)
+function plot_3d_trajectory1(; sim_time_obs::Observable, sim_state_obs::Observable, duration=10.0, dt=0.01, frame_rate=25)
 
     step_count::Integer = convert(Integer, duration / dt)
 
     n_skip_frames::Int8 = convert(Int8, 100 / frame_rate)
 
     for i in 1:n_skip_frames:step_count
-        max_range = 2.0
-        dist = max_range / 2
 
-        x_pos::Float64 = 0.0
-        y_pos::Float64 = df[!, "y"][i]
-        z_pos::Float64 = df[!, "z"][i]
+        # stop simulation is stop button is pressed
+        if sim_state_obs[] == true
 
-        x_low::Float64 = x_pos - dist
-        y_low::Float64 = y_pos - dist
-        z_low::Float64 = z_pos - dist
+            max_range = 2.0
+            dist = max_range / 2
 
-        x_high::Float64 = x_pos + dist
-        y_high::Float64 = y_pos + dist
-        z_high::Float64 = z_pos + dist
+            x_pos::Float64 = 0.0
+            y_pos::Float64 = df[!, "y"][i]
+            z_pos::Float64 = df[!, "z"][i]
 
-        # set the time observable
-        sim_time::Float64 = round(df[!, "timestamp"][i], digits=2)
-        sim_time_obsv[] = sim_time
+            x_low::Float64 = x_pos - dist
+            y_low::Float64 = y_pos - dist
+            z_low::Float64 = z_pos - dist
 
-        # set timeline slider value 
-        set_close_to!(timeline_slider, sim_time)
+            x_high::Float64 = x_pos + dist
+            y_high::Float64 = y_pos + dist
+            z_high::Float64 = z_pos + dist
 
-        vis_ax.limits = (x_low, x_high, y_low, y_high, z_low, z_high)
-        translate!(m, Vec3f(x_pos, y_pos, z_pos))
+            # set the time observable
+            sim_time::Float64 = round(df[!, "timestamp"][i], digits=2)
+            sim_time_obs[] = sim_time
 
-        rotate!(m, Vec3f(1, 0, 0), rad2deg(df[!, "θ"][i]))
+            # set timeline slider value 
+            set_close_to!(timeline_slider, sim_time)
 
-        sleep(n_skip_frames * dt)
+            vis_ax.limits = (x_low, x_high, y_low, y_high, z_low, z_high)
+            translate!(m, Vec3f(x_pos, y_pos, z_pos))
+
+            rotate!(m, Vec3f(1, 0, 0), rad2deg(df[!, "θ"][i]))
+
+            sleep(n_skip_frames * dt)
+
+        end
     end
 
 
